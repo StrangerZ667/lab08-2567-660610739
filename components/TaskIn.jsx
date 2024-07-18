cc1ca869c17282f72b0078995604715dd3cbb517
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-07-18 15:37:59 +0700

    Delete components directory

--- components/TaskIn.jsx
+++
@@ -1,9 +0,0 @@
-function TaskInput(){
-  return(
-    <div className="d-flex gap-1">
-          <input className="form-control" placeholder="Insert a task here.."></input>
-          <button className="btn btn-primary">Add</button>
-    </div>
-  )
-}
-export default TaskInput
